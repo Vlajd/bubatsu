2062419521bb0ca550da95cab43af974852401df
Vlajd <boriskostadinov2005@gmail.com>
2022-09-06 14:18:05 +0200

    Removed debug files 	modified:   Bubatsu/src/Bubatsu/Core/EntryPoint.h 	deleted:    Sandbox/Debug/BubatsuProfile-Runtime.json 	deleted:    Sandbox/Debug/BubatsuProfile-Shutdown.json 	deleted:    Sandbox/Debug/BubatsuProfile-Startup.json 	modified:   .gitignore

--- Bubatsu/src/Bubatsu/Core/EntryPoint.h
+++ Bubatsu/src/Bubatsu/Core/EntryPoint.h
@@ -7,15 +7,15 @@
     {
         Bubatsu::Log::Init();
 
-        BBZ_PROFILE_BEGIN_SESSION("Startup", "Debug/BubatsuProfile-Startup.json");
+        BBZ_PROFILE_BEGIN_SESSION("Startup", "Debug/BubatsuProfile-Startup.dbg.json");
         auto app = Bubatsu::BubatsuMain();
         BBZ_PROFILE_END_SESSION();
 
-        BBZ_PROFILE_BEGIN_SESSION("Runtime", "Debug/BubatsuProfile-Runtime.json");
+        BBZ_PROFILE_BEGIN_SESSION("Runtime", "Debug/BubatsuProfile-Runtime.dbg.json");
         app->Run();
         BBZ_PROFILE_END_SESSION();
 
-        BBZ_PROFILE_BEGIN_SESSION("Shutdown", "Debug/BubatsuProfile-Shutdown.json");
+        BBZ_PROFILE_BEGIN_SESSION("Shutdown", "Debug/BubatsuProfile-Shutdown.dbg.json");
         delete app;
         BBZ_PROFILE_END_SESSION();
 
